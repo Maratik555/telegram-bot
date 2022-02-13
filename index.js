9b6d6717ef99e67422efd0f46082490e5a723d9f
Маратик555 <amr404@bk.ru>
2022-02-13 14:52:28 +0300

    ok

--- index.js
+++ index.js
@@ -1,33 +1,41 @@
-const TelegramApi = require('node-telegram-bot-api');
+const TelegramApi = require('node-telegram-bot-api')
+require('dotenv').config()
 
-const token = '5202250704:AAEQ0161zlcnn_WQCybVb1kKOfFyfa-Wmnc'
+// const token = 'process.env.BOT_TOKEN'
 
-const bot = new TelegramApi(token,{polling:true})
-const {gameOptions,againOptions} = require('/options')
+const bot = new TelegramApi(process.env.BOT_TOKEN,{polling:true})
+const {gameOptions,againOptions} = require('./options')
 const chats = {}
 
 
 
 const startGame = async (chatId) => {
   await bot.sendMessage(chatId,`Сейчас я загадаю тебе цифру от 0 до 9, а ты должен её угадать!`)
-  const randomNumber = Math.floor(Math.random() * 10)
-  chats[chatId] = randomNumber
+  chats[chatId] = Math.floor(Math.random() * 10)
   await bot.sendMessage(chatId,'Отгадывай дружок', gameOptions)
 }
 
 const start = () => {
   bot.setMyCommands([
-    {command: '/start', description:'Начальное привествие'},
-    {command: '/info', description:'Инфо о пользователе'},
-    {command: '/game', description:'Игра'},
+    {command: '/start', description:'Приветствие'},
+    {command: '/info', description: 'Инфо о пользователе'},
+    {command: '/game', description: 'Игра'},
   ])
 
   bot.on('message',async msg => {
+    const sticker = msg.sticker
+    const emoji = msg.emojis
     const text = msg.text
     const chatId = msg.chat.id
+    if (sticker) {
+      return bot.sendMessage(chatId,'👍')
+    }
+    if (emoji) {
+      return bot.sendMessage(chatId,'👍')
+    }
     if(text === '/start') {
-      await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/1df/82b/1df82bcc-cb4f-343c-a961-3417125411e0/1.webp')
-      return  bot.sendMessage(chatId,'Добро пожаловать в нашего бота ! ')
+      return bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/5.webp')
+      // return bot.sendMessage(chatId,'Добро пожаловать в нашего бота ! ')
     }
     if(text === '/info') {
       return bot.sendMessage(chatId,`Тебя зовут ${msg.from.first_name} ${msg.from.last_name}`)
@@ -35,6 +43,7 @@ const start = () => {
     if(text === '/game') {
       return startGame(chatId)
     }
+    await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/256/16.webp')
     return bot.sendMessage(chatId,'Я не понимаю тебя, попробуй ещё раз!)')
   })
 
@@ -44,9 +53,11 @@ const start = () => {
     if(data === '/again') {
       return startGame(chatId)
     }
-    if(data === chats[chatId]) {
+    if(data == chats[chatId]) {
+      await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/10.webp')
       return await bot.sendMessage(chatId,'Поздравляю ты отгадал цифру ' + chats[chatId], againOptions)
     } else {
+      await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/12.webp')
       return bot.sendMessage(chatId,'К сожалению ты не угадал, бот загадал цифру ' + chats[chatId], againOptions)
     }
   })
