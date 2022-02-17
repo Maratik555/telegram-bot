eca0fe1314e27052eec938c462f022556097a786
Marat <80047266+Maratik555@users.noreply.github.com>
2022-02-17 19:13:27 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const {gameOptions,againOptions} = require('./options')
 const chats = {}
 
 
-const startGame = async (chatId) => {
-  await bot.sendMessage(chatId,`Сейчас ${msg.from.first_name}, я загадаю тебе цифру от 0 до 9, а ты должен её угадать!) `, gameOptions)
+const startGame = async (chatId,msg) => {
+  await bot.sendMessage(chatId, `Сейчас ${msg.from.first_name} я загадаю тебе цифру от 0 до 9, а ты должен её угадать!) `, gameOptions)
   chats[chatId] = Math.floor(Math.random() * 10)
 }
 
@@ -28,17 +28,17 @@ const start = () => {
       return bot.sendMessage(chatId,'👍')
     }
     if (text === 'Привет') {
-      return bot.sendMessage(chatId,'Привет 👍')
+      return bot.sendMessage(chatId, `Привет ${msg.from.first_name} 👍`)
     }
     if (photo) {
       return bot.sendMessage(chatId,'😜')
     }
     if(text === '/start' || text === '/start@amr555JS_bot') {
       await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/5.webp')
-      return bot.sendMessage(chatId,`Приветствую тебя ${msg.from.first_name} !`)
+      return bot.sendMessage(chatId,`Приветствую тебя ${msg.from.first_name}!`)
     }
     if(text === '/info' || text === '/info@amr555JS_bot') {
-      return bot.sendMessage(chatId,` Тебя зовут ${msg.from.first_name} !`)
+      return bot.sendMessage(chatId,` Тебя зовут ${msg.from.first_name}!`)
     }
     if(text === '/game' || text === '/game@amr555JS_bot') {
       return startGame(chatId)
@@ -47,10 +47,10 @@ const start = () => {
       return bot.sendMessage(chatId,`Оправь мне свой любимый стикер ${msg.from.first_name} !`)
     }
     return bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/256/16.webp')
-    // return bot.sendMessage(chatId,'Я не понимаю тебя ${msg.from.first_name}, попробуй ещё раз!)')
+    // return bot.sendMessage(chatId,'Я не понимаю тебя, попробуй ещё раз!)')
   })
 
-  bot.on('callback_query', async function(msg){
+  bot.on('callback_query', async function (msg) {
     const data = msg.data
     const chatId = msg.message.chat.id
     if(data === '/again') {
@@ -58,10 +58,10 @@ const start = () => {
     }
     if(data == chats[chatId]) {
       await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/10.webp')
-      return bot.sendMessage(chatId,'Поздравляю ! Молодец ${msg.from.first_name}, ты отгадал цифру ' + chats[chatId] + ' !!!' , againOptions)
+      return bot.sendMessage(chatId, `Поздравляю  ${msg.from.first_name} ! Бот действительно загадал цифру ${chats[chatId]} !!!`, againOptions)
     } else {
       await bot.sendSticker(chatId,'https://tlgrm.ru/_/stickers/a02/150/a02150f8-6e7d-4269-aa3b-2097aafc2f32/12.webp')
-      return bot.sendMessage(chatId,'К сожалению ${msg.from.first_name} ты не угадал, бот загадал цифру ' + chats[chatId], againOptions)
+      return bot.sendMessage(chatId, `К сожалению ${msg.from.first_name} не угадал, бот загадал цифру ${chats[chatId]}`, againOptions)
     }
   })
 }
